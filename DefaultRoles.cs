7a1c2e5f146368ed351ed25fa723865e283e661f
Mohmed Shawky <57459035+mohamedshawky70@users.noreply.github.com>
2025-01-28 23:44:16 +0200

    Add files via upload

---
+++ DefaultRoles.cs
@@ -0,0 +1,18 @@
+﻿using BookHaven.Web.Consts;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookHaven.Web.Seeds
+{
+	public class DefaultRoles
+	{
+		public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+		{
+			if (!roleManager.Roles.Any())
+			{
+				await roleManager.CreateAsync(new IdentityRole(AppRoles.Admin));
+				await roleManager.CreateAsync(new IdentityRole(AppRoles.Achieve));
+				await roleManager.CreateAsync(new IdentityRole(AppRoles.Reception));
+			}
+		}
+	}
+}
---
+++ DefaultUsers.cs
@@ -0,0 +1,25 @@
+﻿using BookHaven.Web.Consts;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookHaven.Web.Seeds
+{
+	public static class DefaultUsers
+	{
+		public static async Task SeedAdminUserAsync(UserManager<ApplicationUser> userManager)
+		{
+			ApplicationUser Admin = new ApplicationUser()
+			{
+				UserName = "MoAdmin",
+				FullName = "MohmedShawky",
+				Email = "MoAdmin@BookHaven.Com",
+				EmailConfirmed = true     //To don't ask to confirme
+			};
+			var user = await userManager.FindByEmailAsync(Admin.Email);
+			if (user is null)
+			{
+				var result = await userManager.CreateAsync(Admin, "P@$$0Rd123");//must strong passowrd
+			    await userManager.AddToRoleAsync(Admin, AppRoles.Admin);
+			}
+		}
+	}
+}
